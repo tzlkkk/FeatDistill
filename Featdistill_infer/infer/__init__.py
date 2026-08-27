ad2b95f665c5d27540accd162b04e29d0b5680cd
tzlkkk <a18882319118@163.com>
2026-08-27 18:06:42 +0800

    Delete Featdistill_infer directory

--- Featdistill_infer/infer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Inference-only release for the FeatDistill four-expert ensemble."""
-
-from .model import FeatDistillEnsemble, Model
-
-__all__ = ["FeatDistillEnsemble", "Model"]
-__version__ = "1.0.0"
